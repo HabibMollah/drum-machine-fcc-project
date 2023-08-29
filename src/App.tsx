e1d1a6c8d059c6a34ee11758d650c60514caf5fe
HabibMollah <hrmollah2002@gmail.com>
2023-08-29 17:30:01 +0600

    make everything functional

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [displayedText, setDisplayedText] = useState('');
+
   useEffect(() => {
     const drumPads = document.querySelectorAll('.drum-pad');
     console.log(drumPads);
@@ -28,56 +30,46 @@ function App() {
     return () => document.removeEventListener('keydown', handleKeyPress);
   }, []);
 
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.currentTarget.focus();
+    e.currentTarget.querySelector('audio')?.play();
+    // const buttonNode = e.target as HTMLButtonElement;
+    // buttonNode.focus();
+    // console.log(buttonNode.innerText);
+    console.log(e.currentTarget.id);
+    // if (typeof e.currentTarget.querySelector('audio') === 'string')
+    setDisplayedText(e.currentTarget.id.toLowerCase());
+  };
+
   return (
     <main id="drum-machine">
-      <div id="display"></div>
+      <div id="display">{displayedText}</div>
       <div className="drum-buttons">
-        <button
-          onClick={() => console.log('Q')}
-          className="drum-pad"
-          id="heater1">
+        <button onClick={handleClick} className="drum-pad" id="heater1">
           Q <audio className="clip" id="Q" src="/Heater-1.mp3"></audio>
         </button>
-        <button
-          onClick={() => console.log('W')}
-          className="drum-pad"
-          id="heater2">
+        <button onClick={handleClick} className="drum-pad" id="heater2">
           W <audio className="clip" id="W" src="/Heater-2.mp3"></audio>
         </button>
-        <button
-          onClick={() => console.log('E')}
-          className="drum-pad"
-          id="heater3">
+        <button onClick={handleClick} className="drum-pad" id="heater3">
           E <audio className="clip" id="E" src="/Heater-3.mp3"></audio>
         </button>
-        <button
-          onClick={() => console.log('A')}
-          className="drum-pad"
-          id="heater4">
+        <button onClick={handleClick} className="drum-pad" id="heater4">
           A <audio className="clip" id="A" src="/Heater-4_1.mp3"></audio>
         </button>
-        <button onClick={() => console.log('S')} className="drum-pad" id="clap">
+        <button onClick={handleClick} className="drum-pad" id="clap">
           S <audio className="clip" id="S" src="/Heater-6.mp3"></audio>
         </button>
-        <button
-          onClick={() => console.log('D')}
-          className="drum-pad"
-          id="openHH">
+        <button onClick={handleClick} className="drum-pad" id="openHH">
           D <audio className="clip" id="D" src="/Dsc_Oh.mp3"></audio>
         </button>
-        <button
-          onClick={() => console.log('Z')}
-          className="drum-pad"
-          id="kickNHat">
+        <button onClick={handleClick} className="drum-pad" id="kickNHat">
           Z <audio className="clip" id="Z" src="/Kick_n_Hat.mp3"></audio>
         </button>
-        <button onClick={() => console.log('X')} className="drum-pad" id="kick">
+        <button onClick={handleClick} className="drum-pad" id="kick">
           X <audio className="clip" id="X" src="/RP4_KICK_1.mp3"></audio>
         </button>
-        <button
-          onClick={() => console.log('C')}
-          className="drum-pad"
-          id="closedHH">
+        <button onClick={handleClick} className="drum-pad" id="closedHH">
           C <audio className="clip" id="C" src="/Cev_H2.mp3"></audio>
         </button>
       </div>
